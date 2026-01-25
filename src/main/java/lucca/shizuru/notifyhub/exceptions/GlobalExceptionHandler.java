190bcf30d980dc73af70f26d7190d6780639135f
Lucca Shizuru <luccashizuru@gmail.com>
2026-01-25 18:47:04 -0300

    refactor/ refatoração para tornar as mensagens de erros mais amigaveis

--- src/main/java/lucca/shizuru/notifyhub/exceptions/GlobalExceptionHandler.java
+++ src/main/java/lucca/shizuru/notifyhub/exceptions/GlobalExceptionHandler.java
@@ -1,39 +1,62 @@
 package lucca.shizuru.notifyhub.exceptions;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import lucca.shizuru.notifyhub.dto.StandardError;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage()));
-        return ResponseEntity.badRequest().body(errors);
+    public ResponseEntity<StandardError> handleValidationErrors(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        var fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .map(f -> new StandardError.ValidationError(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        StandardError err = new StandardError(
+                Instant.now(), HttpStatus.BAD_REQUEST.value(), "Erro de Validação",
+                "Verifique os campos enviados", request.getRequestURI(), fieldErrors);
+
+        log.warn("⚠️ Falha de validação em {}: {}", request.getRequestURI(), fieldErrors);
+        return ResponseEntity.badRequest().body(err);
 
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<Map<String, String>> handleEnumErrors(HttpMessageNotReadableException ex) {
-        System.err.println("Erro na leitura do JSON: " + ex.getMessage());
-
-        Map<String, String> error = new HashMap<>();
+    public ResponseEntity<StandardError> handleEnumErrors(HttpMessageNotReadableException ex,  HttpServletRequest request) {
+        String userMassage = "erro na formatação de JSON.";
 
         if (ex.getMessage() != null && ex.getMessage().contains("NotificationChannel")) {
-            error.put("error", "Canal de envio não suportado. Use EMAIL ou SMS.");
-        } else {
-            error.put("error", "Erro na formatação do JSON enviado.");
+            userMassage = "Canal de envio não suportado. Use EMAIL ou SMS.";
         }
+        StandardError err = new StandardError(
+                Instant.now(), HttpStatus.BAD_REQUEST.value(), "JSON Inválido",
+                userMassage, request.getRequestURI(), null);
 
-        return ResponseEntity.badRequest().body(error);
+        log.error("Erro na leitura do JSON em {}: {}", request.getRequestURI(), ex.getMessage());
+        return ResponseEntity.badRequest().body(err);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<StandardError> handleBusinessError(IllegalArgumentException ex, HttpServletRequest request) {
+        StandardError err = new StandardError(
+                Instant.now(), HttpStatus.BAD_REQUEST.value(), "Regra de Negócio",
+                ex.getMessage(), request.getRequestURI(), null);
+
+        log.error("Violação de regra: {}", ex.getMessage());
+
+
+        return ResponseEntity.badRequest().body(err);
     }
 }
