e3b723877752f63f110dc5699bf6bc9f1cb5906b
Lucca Shizuru <luccashizuru@gmail.com>
2026-01-25 15:36:19 -0300

    refactor/ refatoração do teste unitario

--- src/test/java/lucca/shizuru/notifyhub/services/NotificationServiceTest.java
+++ src/test/java/lucca/shizuru/notifyhub/services/NotificationServiceTest.java
@@ -1,7 +1,11 @@
 package lucca.shizuru.notifyhub.services;
 
 import lucca.shizuru.notifyhub.domain.Notification;
+import lucca.shizuru.notifyhub.domain.enums.NotificationChannel;
+import lucca.shizuru.notifyhub.domain.enums.NotificationPriority;
 import lucca.shizuru.notifyhub.domain.enums.NotificationStatus;
+import lucca.shizuru.notifyhub.dto.NotificationRequestDto;
+import lucca.shizuru.notifyhub.factory.NotificationFactory;
 import lucca.shizuru.notifyhub.repositories.NotificationRepository;
 import lucca.shizuru.notifyhub.services.strategies.NotificationStrategy;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,28 +30,39 @@ class NotificationServiceTest {
     @Mock
     private NotificationStrategy emailStrategy;
 
+    @Mock
+    NotificationFactory notificationFactory;
+
     private NotificationService service;
 
     @BeforeEach
     void setup() {
 
-        service = new NotificationService(repository, List.of(emailStrategy));
+        service = new NotificationService(repository,notificationFactory, List.of(emailStrategy));
     }
 
     @Test
     @DisplayName("Deve processar notificação com sucesso")
     void deveProcessarComSucesso() {
+        NotificationRequestDto dto = new NotificationRequestDto(
+                "Conteúdo teste",
+                NotificationChannel.EMAIL,
+                NotificationPriority.HIGH,
+                "teste@email.com"
+        );
+
         Notification notification = new Notification();
+        notification.setChannel(NotificationChannel.EMAIL);
+        notification.setDestination("teste@email.com");
 
-        notification.setChannel(lucca.shizuru.notifyhub.domain.enums.NotificationChannel.EMAIL);
-
-
+        when(notificationFactory.createNotification(any())).thenReturn(notification);
         when(emailStrategy.isApplicable(any())).thenReturn(true);
         when(repository.save(any())).thenReturn(notification);
 
-        service.scheduleNotification(notification);
+        service.scheduleNotification(dto);
 
         assertEquals(NotificationStatus.SENT, notification.getStatus());
         verify(emailStrategy, times(1)).sendNotification(notification);
+        verify(repository, times(1)).save(notification);
     }
 }
