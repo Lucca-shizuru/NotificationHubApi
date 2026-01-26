5c651d78e87a7fb17fb885ecaad72ad16161d3cf
Lucca Shizuru <luccashizuru@gmail.com>
2026-01-25 22:25:36 -0300

    Refactor/ implementando o JWT no swagger

--- src/main/java/lucca/shizuru/notifyhub/config/OpenApiConfig.java
+++ src/main/java/lucca/shizuru/notifyhub/config/OpenApiConfig.java
@@ -1,8 +1,12 @@
 package lucca.shizuru.notifyhub.config;
 
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +19,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("NotifyHub API")
                         .version("1.0")
-                        .description("Sistema centralizado para agendamento e envio de notificações via E-mail e SMS.")
-                        .contact(new Contact()
-                                .name("Lucca")
-                                .email("shizurulucca.dev@gmail.com")));
+                        .description("Sistema centralizado para agendamento e envio de notificações via E-mail e SMS."))
+                        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new Components()
+                                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                                .name("bearerAuth")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")));
     }
 }
