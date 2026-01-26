b47a709e00fb43c5c38e80a1e9d501544eac526c
Lucca Shizuru <luccashizuru@gmail.com>
2026-01-25 21:06:36 -0300

    feature/ criação do authorizationService

---
+++ src/main/java/lucca/shizuru/notifyhub/services/AuthorizationService.java
@@ -0,0 +1,19 @@
+package lucca.shizuru.notifyhub.services;
+
+import lucca.shizuru.notifyhub.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
